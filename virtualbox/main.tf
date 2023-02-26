7918b4beeaf78b8c8922a59294ec26bd113244a6
Kazimierz Daszkiewicz <daszkiewicz.kazimierz@gmail.com>
2023-02-26 20:16:00 +0100

    remove user data

--- virtualbox/main.tf
+++ virtualbox/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     virtualbox = {
-      source = "terra-farm/virtualbox"
-      version = "0.2.1"
+      source =  "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
     }
   }
 }
@@ -13,7 +13,7 @@ resource "virtualbox_vm" "node" {
   image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
-  user_data = file("${path.module}/user_data")
+  #user_data = file("${path.module}/user_data")
 
   network_adapter {
     type           = "hostonly"
